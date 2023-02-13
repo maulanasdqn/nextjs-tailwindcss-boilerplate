eab3083b429046cee08271fe82641d376f4bba92
Fenny Oktaviani <fnny04@gmail.com>
2023-02-13 17:25:32 +0700

    Fenny Oktaviani | Fix Responsive

--- pages/auth/register.tsx
+++ pages/auth/register.tsx
@@ -9,17 +9,14 @@ const Register: NextPage = (): ReactElement => {
     <AuthLayouts title="Create your Account">
       <form className="space-y-4 md:space-y-6">
         <div>
-          <label
-            htmlFor="email"
-            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-          >
+          <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 ">
             Your email
           </label>
           <input
             type="email"
             name="email"
             id="email"
-            className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
             placeholder="name@company.com"
             required
           />
@@ -41,10 +38,7 @@ const Register: NextPage = (): ReactElement => {
           />
         </div>
         <div>
-          <label
-            htmlFor="password"
-            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-          >
+          <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900">
             Confirm Password
           </label>
           <input
@@ -52,7 +46,7 @@ const Register: NextPage = (): ReactElement => {
             name="password"
             id="password"
             placeholder="••••••••"
-            className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
             required
           />
         </div>
